c0730c5e6b3829b98011ffe9780a5faa64c45bea
Sam Kizzy <samkizzy@Sams-MBP.lan>
2021-04-12 17:38:39 -0500

    First commit

---
+++ v.py
@@ -0,0 +1,10 @@
+#!/Library/Frameworks/Python.framework/Versions/3.8/bin/python3
+print(24 + 25)
+print(23 * 12)
+print(10 + 0.5)
+price = 40
+print(price + 5)
+sales = 5
+print(price - sales)
+current = 35
+print(f'The original price was ${price}. With a sale of ${sales}, your current price is ${current}.')
